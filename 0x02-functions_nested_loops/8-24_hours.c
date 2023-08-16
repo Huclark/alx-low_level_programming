4ad00680ea45e28b001b0b49ff0c40c6005b7b60
huclark <vhuclark@gmail.com>
2023-08-16 01:29:05 +0000

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0 ; a < 24 ; a++)
+	{
+		for (b = 0 ; b < 60 ; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+	}
+}
