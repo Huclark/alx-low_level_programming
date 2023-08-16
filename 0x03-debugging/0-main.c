6de54e616d2ef2bfcd68f4ee507f9944cdbb621b
huclark <vhuclark@gmail.com>
2023-08-16 13:40:10 +0000

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int integer = 0;
+
+	positive_or_negative(integer);
+	return (0);
+}
