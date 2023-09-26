294919b4884609ffe665b26314e208edc19a1d50
Huclark <vhuclark@gmail.com>
2023-09-26 14:56:00 +0000

    Task 10 code modified

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = node_navigator->next;
+		*head = (*head)->next;
 		free(node_navigator);
 		return (1);
 	}
