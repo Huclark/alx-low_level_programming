46f84a5418843e368e317461781304751b88eb8d
Huclark <vhuclark@gmail.com>
2023-10-02 20:22:48 +0000

    task 2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@
 /* Custom Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
