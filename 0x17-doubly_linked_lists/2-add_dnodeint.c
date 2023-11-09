55ac15dfbf738d89b5d30b052eb93625f626c1b8
Huclark <vhuclark@gmail.com>
2023-11-09 15:39:47 +0000

    task 2 corrected

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,9 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 
 	if (!*head)
-		*head = new_node;
+		(*head)->prev = new_node;
 
-	(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
